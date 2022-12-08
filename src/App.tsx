baec4c5cf9c3ece269d028a61284b53b266668b7
agu <iba.agustin@gmail.com>
2022-12-08 00:01:52 -0300

    Choose buttons, playing with Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
+import { Choose } from "./Components/ChooseAction/Choose";
 import "./App.css";
 
 function App() {
-  return <div className="font-bold underline">hola boys</div>;
+  return (
+    <div>
+      <Choose />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/ChooseAction/Choose.tsx
+++ src/Components/ChooseAction/Choose.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function Choose() {
-  return <div></div>;
+function Choose() {
+  return (
+    <div className="flex flex-row items-center justify-center mt-10 ">
+      <button className="border mr-4 w-40 h-32">Create talents sets</button>
+      <button className="border w-40 h-32">View/Edit an existing set</button>
+    </div>
+  );
 }
+
+export { Choose };
