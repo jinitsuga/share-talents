127cf8c84d1ea0a0ad72888de55e5b0fa6692d2c
agu <iba.agustin@gmail.com>
2022-12-16 19:40:54 -0300

    class colors fix, details expand on hover

--- src/Components/SideBuilds/SideItem.tsx
+++ src/Components/SideBuilds/SideItem.tsx
@@ -9,7 +9,13 @@ interface Props {
 export const SideItem: FC<Props> = ({ build }) => {
   const [detailsShown, setDetailsShown] = React.useState(false);
 
-  const background = build.class.toLowerCase();
+  let background = build.class.toLowerCase();
+  if (build.class == "Death Knight") {
+    background = "dk";
+  }
+  if (build.class == "Demon Hunter") {
+    background = "dh";
+  }
 
   const handleEnter = () => {
     setDetailsShown(true);
@@ -22,6 +28,13 @@ export const SideItem: FC<Props> = ({ build }) => {
     <div className={`w-80 flex flex-col rounded bg-${background} m-2 p-1`}>
       <h4 className="text-sm">{build.class} build</h4>
       <span className="text-xs">qweeEEEEEEEEEEEERRRR444123yyu</span>
+      <span
+        onMouseEnter={handleEnter}
+        onMouseLeave={handleLeave}
+        className="text-xs"
+      >
+        + details
+      </span>
       <Tooltip
         text="some random details about the build"
         shown={detailsShown}
