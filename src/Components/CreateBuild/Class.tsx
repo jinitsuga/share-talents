4f68b98c5908767591a7018427db7aaa988207e5
agu <iba.agustin@gmail.com>
2023-01-09 14:09:25 -0300

    class block fix from div to button

--- src/Components/CreateBuild/Class.tsx
+++ src/Components/CreateBuild/Class.tsx
@@ -10,13 +10,13 @@ interface Props {
 
 const Class: FC<Props> = ({ name, icon, color, text, onClick }) => {
   return (
-    <div
+    <button
       onClick={onClick}
       className={`${color} ${text} text-lg w-32 h-14 flex flex-col rounded m-2 
       justify-center items-center text-center hover:cursor-pointer hover:border hover:scale-115 active:bg-slate-700`}
     >
       <span>{name}</span>
-    </div>
+    </button>
   );
 };
 
